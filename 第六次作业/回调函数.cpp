d4dc63857535f7a5cbeaaf935fb47bf29c5916a0
System Administrator <root@promote.cache-dns.local>
2020-04-01 16:00:59 +0800

    seventh

---
+++ 第六次作业/回调函数.cpp
@@ -0,0 +1,72 @@
+//实现功能：既能排序整型数，也可以排序字符串
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int cmpGT(const int *p1, const int *p2)
+{
+//实现两数比较，如果大于，则返回1，等于则返回0，小于则返回-1
+ if(*p1>*p2) return 1;
+    if(*p1==*p2) return 0;
+    if(*p1<*p2) return -1;
+    return 0;
+
+}
+int cmpLT(const int *p1, const int *p2)
+{
+//实现两数比较，如果大于，则返回-1，等于则返回0，小于则返回1
+if(*p1>*p2) return -1;
+    if(*p1==*p2) return 0;
+    if(*p1<*p2) return 1;
+    return 0;
+
+
+}
+
+void swap(int *p1, int *p2)
+{
+//实现两个整形变量的值交换
+int temp;
+    temp=*p1;
+    *p1=*p2;
+    *p2=temp;
+
+}
+
+void bubble(int *base,  int(*cmp)(const int *, const int *))
+{
+//请在这里补充冒泡排序，请根据main函数中对bubble函数的调用，分析形参的作用
+ int i=0;
+    int j=0;
+    for(i=0;i<9;i++){
+        for(j=0;j+1<9-i+1;j++){
+            if(cmp(&base[j],&base[j+1])==1){
+                swap(&base[j],&base[j+1]);
+            }
+        }
+    }
+
+
+}
+
+int main()
+{
+	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+	int i = 0;
+	bubble(arr,  cmpGT);
+	for (i = 0; i < 10; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+
+	bubble(arr,  cmpLT);
+	for (i = 0; i < 10; i++)
+	{
+		cout<<arr[i]<<" ";
+	}	
+	cout<<endl;
+	return 0;
+}
+
---
+++ 第六次作业/对象数组.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CStudent
+{
+private:
+    string name;
+    int year;
+public:
+    static int count;
+    static int getcount(){
+        return count;
+    }
+    void printname(){
+        cout << "name:";
+        cout << name<< endl;
+    }
+    CStudent(string a="noname",int b=0){
+        name=a;
+        year=b;
+        cout << name << " is contructing" << endl;
+        count++;
+    }
+    ~CStudent(){
+        count--;
+        cout << name << " is destructing" << endl;
+    }
+
+};
+//请在这里初始静态成员变量
+int CStudent::count=0;
+
+
+int main()
+{
+	cout << "现在有学生" << CStudent::getcount() << endl;
+	{
+		CStudent stuB[5];
+		stuB[4].printname();
+		cout << "现在有学生" << CStudent::getcount() << endl;
+	}
+	cout << "现在有学生" << CStudent::getcount() << endl;
+	{
+		CStudent stuA[2] = {CStudent("Tom", 3), CStudent("Jerry", 2)};
+		for (int i = 0; i < 2; i++)
+		{
+			stuA[i].printname();
+		}
+		cout << "现在有学生" << CStudent::getcount() << endl;
+	}
+	cout << "现在有学生" << CStudent::getcount() << endl;
+
+	return 0;
+}
+
