a6233c7f85d9bc193569248761a9366d35fad691
System Administrator <root@promote.cache-dns.local>
2020-04-14 18:47:04 +0800

    nineth

---
+++ 第八次作业/sourcecode (3).cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+const int pi=3.14159;
+class Shape{
+private:
+    int m_ID;
+public:
+    static int count;
+    void setid(int a){
+        m_ID=a;
+    }
+    int getid(void){
+        return m_ID;
+    }
+    int getArea(){
+        return 0;
+    }
+    Shape(int a){
+        count++;
+        m_ID=a;
+        cout << "Shape constructor called:" << count <<endl;
+    }
+    ~Shape(){
+        count--;
+        cout << "Shape destructor called:" << count << endl;
+    }
+};
+int Shape::count=0;
+class Circle:public Shape
+{
+private:
+    int r;
+public:
+    int getr(){
+        return r;
+    }
+    void setr(int a){
+        r=a;
+    }
+    int getArea(){
+        return r*r*pi;
+    }
+    Circle(int a,int b):Shape(b){
+        r=a;
+        //count++;
+        cout <<"Circle constructor called:" << count << endl;
+    }
+    ~Circle(){
+        count--;
+        cout << "Circle destructor called:" << count << endl;
+        count++;
+    }
+};
+class Rectangle:public Shape
+{
+private:
+    int h,w;
+public:
+    int geth(){
+        return h;
+    }
+    int getw(){
+        return w;
+    }
+    void set(int a,int b){
+        h=a;
+        w=b;
+    }
+    int getArea(){
+        return h*w;
+    }
+    Rectangle(int a,int b,int c):Shape(c){
+        //count++;
+        h=a;
+        w=b;
+        cout << "Rectangle constructor called:" << count <<endl;
+    }
+    ~Rectangle(){
+        //count--;
+        
+        cout << "Rectangle destructor called:" << count << endl;
+        count++;
+    }
+};
+
+int main()
+{
+    Shape s(1);//1表示ID
+    Circle c(4,2);//4表示半径，2表示ID
+    Rectangle r(4, 5,3);//4和5表示长和宽，3表示ID
+    cout<<"Shape的面积"<<s.getArea()<<endl;
+    cout<<"Circle的面积"<<c.getArea()<<endl;
+    cout<<"Rectangle的面积"<<r.getArea()<<endl;
+}
+
---
+++ 第八次作业/sourcecode (4).cpp
@@ -0,0 +1,73 @@
+#include "iostream"
+using namespace std;
+
+class vehicle{
+private:
+    int maxspeed;
+    int weight;
+public:
+    void run(){
+        cout << "vehicle run" <<endl;
+
+    }
+    void stop(){
+        cout << "vehicle stop" << endl;
+    }
+    vehicle(int a,int b){
+        maxspeed=a;
+        weight=b;
+        cout << "vehicle constructor called. "<<"maxspeed:"<<maxspeed<<"; "<<"weight"<<weight << endl;
+    }
+    ~vehicle(){
+        cout <<"vehicle destructor called. "  <<"maxspeed:"<<maxspeed<<"; "<<"weight"<<weight<<endl;
+    }
+};
+class bicycle:virtual public vehicle
+{
+private:
+    int height;
+public:
+    bicycle(int a,int b,int c):vehicle(b,c){
+        height=a;
+        cout << "bicycle constructor called. ";
+        cout << "height:"<<height<<endl;
+    }
+    ~bicycle(){
+        cout << "bicycle destructor called. ";
+        cout << "height:"<< height<<endl;
+    }
+};
+class motorcar:virtual public vehicle
+{
+    private:
+    int setnum;
+    public:
+    motorcar(int a,int b,int c):vehicle(b,c){
+        setnum=a;
+        cout << "motorcar constructor called. ";
+        cout <<"setnum:"<< setnum<<endl;
+    }
+    ~motorcar(){
+        cout << "motorcar destructor called. ";
+        cout << "setnum:"<<setnum<<endl;
+    }
+};
+class motorcycle:public bicycle,public motorcar{
+    public:
+    motorcycle(int a,int b,int c,int d):bicycle(a,c,d),motorcar(b,c,d),vehicle(c,d){
+        cout << "motorcycle constructor called"<<endl;
+    }
+    ~motorcycle(){
+        cout << "motorcycle destructor called"<<endl;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    motorcycle m(1, 2, 3, 4);//1表示height，2表示setnum，3表示maxspeed，4表示weight
+    m.run();
+    m.stop();
+    return 0;
+}
+
