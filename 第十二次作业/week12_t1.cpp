37c5cb2350f69ab6237ebe2352e89343c91eb194
zhuchenxi1 <18994684012@163.com>
2020-05-21 15:40:48 +0800

    13th

---
+++ 第十二次作业/week12_t1.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+template <typename T>
+class Array
+{
+  public:
+ vector <T> a;  //
+    Array(int n){}
+ void input(int n){
+   int i=0;
+   while(i<n){
+     T k;
+     cin >> k;
+     a.push_back(k);
+       i++;
+   }
+ }
+T & operator[](int n)
+{
+  return a[n];
+}
+
+};
+
+int gcd(int a,int b)
+{
+    if(!b)
+        return a;
+    return gcd(b,a%b);
+}
+
+    
+
+int max(int a,int b){
+    if(a>b) return a;
+    return b;
+}
+int min(int a,int b){
+    if(a<b) return a;
+    return b;
+}
+
+class Fract
+{
+  public:
+  int n,m;
+  Fract(int a=0,int b=1):n(a),m(b){
+    
+      if(a<0) a=-a;
+      if(b<0) b=-b;
+      int c;
+      c=gcd(max(a,b),min(a,b));
+      if(m<0){
+          n=-n;
+          m=-m;
+      }
+      if(c!=1){
+          n=n/c;
+          m=m/c;
+      }
+  }
+    void show(){
+        if(n==0||m==1) cout << n << endl;
+        else{
+            cout << n << '/' << m << endl;
+        }
+    }
+    Fract &operator +=(Fract &x){
+    Fract y(x.n*m+x.m*n,x.m*m);
+    return *this=y;
+
+}
+    
+   friend istream &operator>>(istream &is,Fract &f)
+   {
+       is>>f.n>>f.m;
+       return is;
+   }
+
+    
+   
+};
+
+int main()
+{
+    unsigned int n;
+    cin >> n;
+    Array<double> db(n);
+    db.input(n);
+    double dbsum(0.0);
+    for (unsigned int i = 0; i < n; i++)
+        dbsum += db[i];
+    cout << dbsum << endl;
+
+    cin >> n;
+    Array<Fract> fr(n);
+    fr.input(n);
+    Fract frsum(0, 1);
+    for (unsigned int i = 0; i < n; i++)
+        frsum += fr[i];
+    frsum.show();
+}
+
+
---
+++ 第十二次作业/week12_t2.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+typedef int elementType;
+typedef struct node
+{
+    elementType data;
+    node* next;
+} LList, *PList;
+
+class linkedList
+{
+public:
+    linkedList();//构造函数
+    virtual ~linkedList();//析构函数，注意要链表中所有节点的资源
+    void insert( int value );//警告：必须初始化才能使用！
+    bool initiate();//初始化单链表，使用new操作创建头结点。如果创建失败，则返回false，否则返回true
+    bool isEmpty();//判断单链表是否为空
+    //删除单链表中第pos个元素结点，并将删除的节点的值存在value中。
+    //注意：如果链表为空、删除位置大于链表长度、以及删除位置为0的情况，需要终止删除并输出相应信息
+    bool remove( int pos, int& value );
+    void print();//顺序打印单链表，如果是单链表为空，则输出 Empty
+    int Length();//返回单链表长度。如果是单链表为空，则返回-1
+private:
+    LList *head;
+    int len;
+};
+
+//请在此处补充所有成员函数的实现
+
+
+linkedList::linkedList(){
+    
+}
+linkedList::~linkedList(){
+    LList *p=head;
+    LList *q;
+    while(p!=nullptr){
+        q=p->next;
+        delete p;
+        p=q;
+    }
+}
+void linkedList::insert(int value){
+    LList *p=new LList();
+    p->data=value;
+    p->next==nullptr;
+    LList *q=head;
+   
+    while(q->next!=nullptr){
+        
+        q=q->next;
+    }
+    q->next=p;
+    
+}
+bool linkedList::initiate(){
+    head= new LList();
+    head ->next=nullptr;
+    return true;
+}
+bool linkedList::isEmpty(){
+    if(head->next!=nullptr) return false;
+    return true;
+}
+bool linkedList::remove(int pos, int &value){
+    int count=1;
+       LList *s=head->next;
+    if(head->next==nullptr) count=0;
+       else{
+           while(s!=nullptr){
+               count++;
+               s=s->next;
+           }
+           
+       }
+    if(pos>count-1){
+        cout <<"pos > len, failed";
+        return false;
+    }
+    if(pos<=0){
+        cout <<"pos <= 0, failed";
+        return false;
+    }
+    else{
+    int count=0;
+    LList *q=head;
+    LList *p=head->next;
+    while(p!=nullptr){
+        count++;
+        if(count==pos){
+            value=p->data;
+            q->next=p->next;
+            delete p;
+            break;
+        }
+        else{
+            p=p->next;
+            q=q->next;
+        }
+    }
+    return true;
+    }
+}
+int linkedList::Length(){
+    int count=1;
+    LList *p=head->next;
+    if(head->next==nullptr) return -1;
+    else{
+        while(p!=nullptr){
+            count++;
+            p=p->next;
+        }
+        return count-1;
+    }
+}
+
+
+
+
+
+
+
+
+//成员函数print
+void linkedList::print()
+{
+    if( isEmpty() )
+    {
+        cout << "Empty" << endl;
+        return;
+    }
+    LList* tmp = head->next;
+    while(tmp)
+    {
+        cout <<tmp->data << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+
+int main(int argc, char* argv[])
+{
+    linkedList L1;
+    int n;
+    int val;
+    //初始化链表
+    if(!L1.initiate())
+        return 0;
+
+    cin>>n;//输入链表中数据个数
+    for(int i=0; i<n; i++) //输入n个数，并插入链表
+    {
+        cin>>val;
+        L1.insert(val);
+    }
+    cout << "Origin Length：" << L1.Length() << endl;//输出链表长度
+    cout << "data：" ;
+    L1.print();//打印链表
+
+    cin>>n;//输入需要删除的数据的位置
+    if (L1.remove(n,val))
+    {
+        //删除位置n的数据，并将删除的数据值放在val中
+        cout<<"Delete the data at position("<<n<<"):"<<val<<endl;
+        cout<< "New Length：" << L1.Length()<< endl;//输出链表长度
+        cout<< "data：" ;
+        L1.print();//打印链表
+    }
+
+    return 0;
+}
+
+
